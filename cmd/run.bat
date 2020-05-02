830fb467976eb6ec524072081d49f834bc7bfebe
kaatinga <kaatinga@gmail.com>
2020-05-02 11:22:28 +0300

    ui simlink data is added

---
+++ cmd/run.bat
@@ -0,0 +1,11 @@
+for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
+     set dow=%%i
+     set month=%%j
+     set day=%%k
+     set year=%%l
+)
+set datestr=%day%.%month%.%year%
+
+go build -ldflags "-X 'main.compileDate=%datestr%'" web/main.go web/log.go web/handlers.go web/middleware.go web/routers.go web/logfuncs.go web/db.go web/models.go
+
+main
